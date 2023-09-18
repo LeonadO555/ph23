28fc5bccde8217c034d84664ba6784fcdc796db9
LeonadO555 <lmikhailov@supplyshift.net>
2023-09-18 12:32:39 +0400

    new project

---
+++ src/main/java/LoginPage.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage {
+    public LoginPage(WebDriver driver) {super(driver);}
+
+    @FindBy(id = "defaultRegisterFormEmail")
+    private WebElement emailInputField;
+
+
+    @FindBy(css = "input[formcontrolname='password']")
+    private WebElement passInputField;
+
+    @FindBy(css = "button[type='submit']")
+    private WebElement loginButton;
+
+    @FindBy(id = "error-message")
+    private WebElement errorMessage;
+
+    public void enterEmail(String emailValue) {emailInputField.sendKeys(emailValue);}
+    public void enterPassword(String passValue) {
+        passInputField.sendKeys(passValue);
+    }
+    public void clickOnLoginButton() {
+        loginButton.click();
+    }
+
+
+    public boolean errorMessageHasText(String expectedText){
+        return errorMessage.getText().contains(expectedText);
+    }
+}
---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,99 @@
+import org.testng.annotations.Test;
+
+import static java.lang.Thread.sleep;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+
+
+public class LoginTest extends BaseTest {
+    @Test
+    public void successLogin() throws InterruptedException {
+        LoginPage loginPage = new LoginPage(app.driver);
+        loginPage.enterEmail("test@gmail.com");
+        loginPage.enterPassword("test@gmail.com");
+        loginPage.clickOnLoginButton();
+        ContactsPage contactsPage = new ContactsPage(app.driver);
+        sleep(1000);
+        assertTrue(contactsPage.contactsPageIsDisplayed());
+    }
+    @Test
+    public void invalidEmail() throws InterruptedException {
+        LoginPage loginPage = new LoginPage(app.driver);
+        loginPage.enterEmail("bullshit@gmail.com");
+        loginPage.enterPassword("test@gmail.com");
+        loginPage.clickOnLoginButton();
+        ContactsPage contactsPage = new ContactsPage(app.driver);
+        sleep(1000);
+        assertFalse(contactsPage.contactsPageIsDisplayed());
+        assertTrue(loginPage.errorMessageHasText(
+                "Please check your activation or Login + Password combination"));
+    }
+    @Test
+    public void invalidPass() throws InterruptedException {
+        LoginPage loginPage = new LoginPage(app.driver);
+        loginPage.enterEmail("test@gmail.com");
+        loginPage.enterPassword("bullshit@gmail.com");
+        loginPage.clickOnLoginButton();
+        ContactsPage contactsPage = new ContactsPage(app.driver);
+        sleep(1000);
+        assertFalse(contactsPage.contactsPageIsDisplayed());
+        assertTrue(loginPage.errorMessageHasText(
+                "Please check your activation or Login + Password combination"));
+    }
+
+}
+
+
+//    @Test
+//    public void invalidPassword() {
+//        LoginPage loginPage = new LoginPage(driver);
+//        loginPage.enterUsername("standard_user");
+//        loginPage.enterPassword("bullshit");
+//        loginPage.clickOnLoginButton();
+//        loginPage.errorMessageIsDisplayed();
+//        assertTrue(loginPage.errorMessageHasText(
+//                "Username and password do not match any user in this service"));
+//    }
+//
+//    @Test
+//    public void loginLockedOutUser() {
+//        LoginPage loginPage = new LoginPage(driver);
+//        loginPage.enterUsername("locked_out_user");
+//        loginPage.enterPassword("secret_sauce");
+//        loginPage.clickOnLoginButton();
+//        loginPage.errorMessageIsDisplayed();
+//        assertTrue(loginPage.errorMessageHasText(
+//                "Sorry, this user has been locked out"));
+//    }
+//
+//    @Test
+//    public void loginInvalidUsername() throws InterruptedException {
+//        LoginPage loginPage = new LoginPage(driver);
+//        loginPage.enterUsername("bullshit");
+//        loginPage.enterPassword("secret_sauce");
+//        loginPage.clickOnLoginButton();
+//        assertTrue(loginPage.errorMessageHasText(
+//                "Username and password do not match any user in this service"));
+//    }
+//
+//    @Test
+//    public void elementsAreDisplayed() {
+//        LoginPage loginPage = new LoginPage(driver);
+//        assertTrue(loginPage.logoIsDisplayed("Swag Labs"));
+//        assertTrue(loginPage.credIsDisplayed());
+//        assertTrue(loginPage.passwordsAreDisplayed());
+//    }
+//    @Test
+//    public void successLogout() throws InterruptedException {
+//        LoginPage loginPage = new LoginPage(driver);
+//        loginPage.successLogin("standard_user", "secret_sauce");
+//        InventoryPage inventoryPage = new InventoryPage(driver);
+//        assertTrue(inventoryPage.inventoryListIsDisplayed());
+//        Header header = new Header(driver);
+//        header.clickOnBurgerMenu();
+//        SideBar sideBar = new SideBar(driver);
+//        sleep(300);
+//        sideBar.followTheLogoutLink();
+//        assertTrue(loginPage.usernameIsDisplayed());
+//    }
