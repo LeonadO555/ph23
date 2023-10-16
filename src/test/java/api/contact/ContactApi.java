36a54d058c5ba291e2d105a4bc41623c3ab21863
Leonid Kazarin <l.kazarin@me.com>
2023-10-16 13:42:18 +0200

    edited new names of contact for AddPhoneTest.java

--- src/test/java/api/contact/ContactApi.java
+++ src/test/java/api/contact/ContactApi.java
@@ -6,7 +6,7 @@ import io.restassured.response.Response;
 import schemas.ContactDTO;
 
 public class ContactApi extends ApiBase {
-
+    String ENDPOINT = "/api/contact";
     ContactDTO dto;
 
     Response response;
@@ -15,6 +15,9 @@ public class ContactApi extends ApiBase {
     String firstName = faker.internet().uuid();
     String lastName = faker.internet().uuid();
     String description = faker.internet().uuid();
+    String editFirstName = faker.internet().uuid();
+    String editLastName = faker.internet().uuid();
+    String editDescription = faker.internet().uuid();
 
     // int number = faker.number().numberBetween(1,9);
     public ContactDTO randomRequestBodyForCreateContact(){
@@ -24,11 +27,32 @@ public class ContactApi extends ApiBase {
         dto.setDescription(description);
         return dto;
     }
+    public ContactDTO randomRequestBodyForEditContact(int id){
+        dto = new ContactDTO();
+        dto.setId(id);
+        dto.setFirstName(editFirstName);
+        dto.setLastName(editLastName);
+        dto.setDescription(editDescription);
+        return dto;
+    }
 
-    public Response createContact(Integer code){
-        String endPoint = "/api/contact";
-        response = postRequest(endPoint, code, randomRequestBodyForCreateContact());
+    public Response createContact(int code){
+        response = postRequest(ENDPOINT, code, randomRequestBodyForCreateContact());
         response.as(ContactDTO.class);
+        return response;
+    }
+
+    public void editContact(int code, int id) {
+        putRequest(ENDPOINT, code, randomRequestBodyForEditContact(id));
+    }
+
+    public Response deleteContact(int code, int id) {
+        response = deleteRequest(ENDPOINT+"/{id}", code, id);
+        return response;
+    }
+
+    public Response getContact(int code, int id) {
+        response = getRequestWithParam(ENDPOINT+"/{id}", code, "id", id);
         return response;
     }
 }
